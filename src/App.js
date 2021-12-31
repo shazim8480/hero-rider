7cbfe211f0da6a09296b41f494fe7d3ab8454064
shazim8480 <troublemaker8480@gmail.com>
2021-12-31 20:38:54 +0600

    react-router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import Home from "./components/Home/Home";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
