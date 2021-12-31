e914b22ba40b2c14a73ae5da56977da0e2590b05
shazim8480 <troublemaker8480@gmail.com>
2021-12-31 19:25:00 +0600

    name and email config

---
+++ src/components/Home/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <h2>This component is for landing page</h2>
+      <h4>home component</h4>
+    </div>
+  );
+};
+
+export default Home;
